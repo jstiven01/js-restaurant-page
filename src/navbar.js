41fcbb247ca4f92b5edf3d25dc3a8c91294be308
jstiven01 <stiven10@gmail.com>
2019-12-26 08:59:24 -0500

    Changing navbar innerHTML to createElement and AppendChild

--- src/navbar.js
+++ src/navbar.js
@@ -1,33 +1,51 @@
 const nav = () => {
-  const element = document.createElement('nav');
+  const parentElement = document.createElement('nav');
   const navClasses = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom navbar-custom-color';
+  parentElement.setAttribute('class', navClasses);
 
-  const navContent = `
-        <div class="d-flex w-50 order-0">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-        <div class="navbar-collapse collapse justify-content-center order-2" id="collapsingNavbar">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" id="home">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="menu">Menu</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <span class="text-truncate mt-1 w-50 text-right order-1 order-md-last"></span>
-      `;
+  const divHead = document.createElement('div');
+  divHead.setAttribute('class', 'd-flex w-50 order-0');
 
+  const button = document.createElement('button');
+  button.setAttribute('class', 'navbar-toggler');
+  button.setAttribute('type', 'button');
+  button.setAttribute('data-toggle', 'collapse');
+  button.setAttribute('data-target', '#collapsingNavbar');
 
-  element.setAttribute('class', navClasses);
-  element.innerHTML = navContent;
-  return element;
+  const spanButton = document.createElement('span');
+  spanButton.setAttribute('class', 'navbar-toggler-icon');
+
+  button.appendChild(spanButton);
+  divHead.appendChild(button);
+
+  const divItems = document.createElement('div');
+  divItems.setAttribute('class', 'navbar-collapse collapse justify-content-center order-2');
+  divItems.setAttribute('id', 'collapsingNavbar');
+
+  const ulItems = document.createElement('ul');
+  ulItems.setAttribute('class', 'navbar-nav');
+  const arrayLinks = ['Home', 'Menu', 'Contact'];
+
+  for (let i = 0; i < arrayLinks.length; i += 1) {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'nav-item');
+    const a = document.createElement('a');
+    a.setAttribute('class', 'nav-link');
+    a.setAttribute('id', arrayLinks[i].toLowerCase());
+    a.innerHTML = arrayLinks[i];
+    li.appendChild(a);
+    ulItems.appendChild(li);
+  }
+
+  divItems.appendChild(ulItems);
+  const spanText = document.createElement('span');
+  spanText.setAttribute('class', 'text-truncate mt-1 w-50 text-right order-1 order-md-last');
+
+  parentElement.appendChild(divHead);
+  parentElement.appendChild(divItems);
+  parentElement.appendChild(spanText);
+
+  return parentElement;
 };
 
 export default nav;
